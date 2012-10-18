17aa96ea59dade1444351aaf9d2f56360099a177
Pavel Rusinov <pa1603ru@rambler.ru>
2012-10-18 17:06:43 +0400

    ||

--- Robot/src/robot/Robot.java
+++ Robot/src/robot/Robot.java
@@ -3,7 +3,7 @@ package robot;
 
 public class Robot {
     private int fact(int x){
-        if(x == 1 | x == 0) {return 1;}
+        if(x == 1 || x == 0) {return 1;}
         return x * fact(x-1);
     }
     
